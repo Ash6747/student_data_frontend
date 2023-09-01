5e1b1aa46e843824a9506b4f9e049f908fef3320
Ashish Satpute <ashishsatpute6747@gmail.com>
2023-09-01 22:47:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Register from './components/Register';
+import Details from './components/Details';
+import {Routes,Route} from "react-router-dom"
+import Edit from './components/Edit';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      {/* <Home/> */}
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/register" Component={Register} />
+        <Route exact path="/edit/:id" Component={Edit} />
+        <Route exact path="/view/:id" Component={Details} />
+      </Routes>
     </div>
   );
 }
